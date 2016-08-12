9ed46d0a2597674edc8b3732c726bd7173c1a5a8
Vincent Chiu <vincent@datanerds.com>
2016-08-12 08:59:56 -0700

    added dropdown in view

--- resources/views/pirate.blade.php
+++ resources/views/pirate.blade.php
@@ -31,6 +31,16 @@
                                 <label for="rank" class="col-sm-2 control-label">Rank</label>
                                 <div class="col-sm-10">
                                     Create dropdown here
+                                    <div class="dropdown">
+                                        <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Dropdown Example
+                                            <span class="caret"></span></button>
+                                        <ul class="dropdown-menu">
+                                            <li><a href="#">HTML</a></li>
+                                            <li><a href="#">CSS</a></li>
+                                            <li><a href="#">JavaScript</a></li>
+                                        </ul>
+                                    </div>
+
                                 </div>
                             </div>
                             <div class="form-group">
