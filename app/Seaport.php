2eed0220c6b89247302209e351cfdf9a259b8c49
Vincent Chiu <vincent@datanerds.com>
2016-08-12 15:39:33 -0700

    added variables to Seaport class

--- app/Seaport.php
+++ app/Seaport.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Seaport extends Model
 {
     //
+//    public $timestamps = false;
+
+    public $attacked_at;
+
+    protected $fillable = [
+        'name',
+        'treasure_amount',
+    ];
+
+//    public function breed()
+//    {
+//        return $this->belongsTo('Furbook\Breed');
+//    }
+
 }
