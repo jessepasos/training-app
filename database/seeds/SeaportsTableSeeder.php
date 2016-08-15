54c3079351de635c4163afd2fd735b4851c9ac3e
Vincent Chiu <vincent@datanerds.com>
2016-08-15 08:20:22 -0700

    added new migration for seaports

---
+++ database/seeds/SeaportsTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SeaportsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('seaports')->insert([
+            'name' => 'Jamaica Bay',
+        ]);
+
+        DB::table('seaports')->insert([
+            'name' => 'Cool Bay',
+        ]);
+
+        DB::table('seaports')->insert([
+            'name' => 'Dragon Bay',
+        ]);
+        
+    }
+}
