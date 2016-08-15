abae140e9ce9770df076d4e64c54620a3ec84fbe
Vincent Chiu <vincent@datanerds.com>
2016-08-15 11:09:32 -0700

    cleaned up more code

--- resources/views/seaport/index.blade.php
+++ resources/views/seaport/index.blade.php
@@ -5,26 +5,21 @@
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading"><b>You are about to embark on an epic journey with the crew of the Black Perl. Only proceed if you think you have what it takes.</b></div>
+                    <div class="panel-heading"><b>You are about to embark on an epic journey with the crew of the Black
+                            Perl. Only proceed if you think you have what it takes.</b></div>
 
                     <div class="panel-body">
-                        {{--<div class="text-center">--}}
-                            {{--<img src="images/the-black-perl.png" />--}}
-                        {{--</div>--}}
                         List of seaports
-
-                        {{--@foreach($seaports as $seaport)'--}}
                         @foreach ($seaports as $seaport)
-                            <li class="list-group-item"><a href="/seaport/{{ $seaport->id }}">
+                            <li class="list-group-item">
+                                <a href="/seaport/{{ $seaport->id }}">
                                     <span class="glyphicon glyphicon glyphicon-pencil" aria-hidden="true"></span>
-
-                               <div> {{ $seaport->name }} </div>
-                                <div>treasure amount: {{ $seaport -> treasure_amount }}</div>
+                                    <div> {{ $seaport->name }} </div>
+                                    <div>treasure amount: {{ $seaport -> treasure_amount }}</div>
                                 </a>
 
                                 {{ Form::open(array('url' => '/seaport/' . $seaport -> id . '/attack')) }}
                                 {{ csrf_field() }}
-                                {{--attack this!--}}
                                 <button type="submit" class="btn btn-primary">Attack this port!</button>
                                 {{ Form::close() }}
                             </li>
@@ -34,7 +29,7 @@
 
                     <div class="panel-footer">
                         {{--<div class="text-center">--}}
-                            {{--<a href="/login" class="btn btn-default">Continue</a>--}}
+                        {{--<a href="/login" class="btn btn-default">Continue</a>--}}
                         {{--</div>--}}
                     </div>
                 </div>
