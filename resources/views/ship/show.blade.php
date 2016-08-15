bac514fa055de9dd702a946f078cc9602812b6c0
Vincent Chiu <vincent@datanerds.com>
2016-08-15 16:06:27 -0700

    fixed show blade for ships

--- resources/views/ship/show.blade.php
+++ resources/views/ship/show.blade.php
@@ -1,8 +1,8 @@
 
 @extends('layouts.app')
 
-{{--<a class="navbar-brand" href="{{ url('/seaport') }}">--}}
-    {{--SEAPORT INDEX--}}
+{{--<a class="navbar-brand" href="{{ url('/ship') }}">--}}
+    {{--ship INDEX--}}
 {{--</a>--}}
 
 
@@ -14,30 +14,30 @@
 
     <div class="container">
 
-        {{--<a class="navbar-brand" href="{{ url('/seaport') }}">--}}
-            {{--SEAPORT INDEX--}}
+        {{--<a class="navbar-brand" href="{{ url('/ship') }}">--}}
+            {{--ship INDEX--}}
         {{--</a>--}}
 
         <div class="row">
 
-            <a class="navbar-brand" href="{{ url('/seaport') }}">
-                SEAPORT INDEX
+            <a class="navbar-brand" href="{{ url('/ship') }}">
+                ship INDEX
             </a>
 
 
-            {{ Form::open(array('url' => '/seaport/' . $seaport -> id . '/attack')) }}
+            {{ Form::open(array('url' => '/ship/' . $ship -> id . '/attack')) }}
             {{ csrf_field() }}
             <button type="submit" class="btn btn-primary">Attack this port!</button>
             {{ Form::close() }}
 
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">{{ $seaport->rank }} {{ $seaport->name }}</div>
+                    <div class="panel-heading">{{ $ship->rank }} {{ $ship->name }}</div>
 
                     <div class="panel-body">
 
 
-                        <h1>Edit seaport</h1>
+                        <h1>Edit ship</h1>
 
                         @if (session('status'))
                             <div class="alert alert-success">
@@ -50,14 +50,14 @@
                             <div class="form-group">
                                 <label for="name" class="col-sm-2 control-label">Name</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control" id="name" name="seaport_name" value="{{ $seaport->name }}">
+                                    <input type="text" class="form-control" id="name" name="ship_name" value="{{ $ship->name }}">
                                 </div>
                             </div>
 
                             <div class="form-group">
                                 <label for="treasure_amount" class="col-sm-2 control-label">treasure_amount</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control" id="treasure_amount" name="seaport_treasure_amount" value="{{ $seaport->treasure_amount }}">
+                                    <input type="text" class="form-control" id="treasure_amount" name="ship_treasure_amount" value="{{ $ship->treasure_amount }}">
                                 </div>
                             </div>
 
