0803312d0828c196e54168b4f61e085aed970336
Vincent Chiu <vincent@datanerds.com>
2016-08-16 10:51:49 -0700

    cleaned up controller

--- app/Http/Controllers/PirateController.php
+++ app/Http/Controllers/PirateController.php
@@ -30,46 +30,26 @@ class PirateController extends Controller
     public function index($id)
     {
         $pirate = Pirate::find($id);
-
         return view('pirate')->withPirate($pirate);
     }
 
     public function store($id, Request $request)
     {
-
         Log::info($request);
         $pirate = Pirate::find($id);
-
         $pirate->name = $request->get('pirate_name');
         $pirate->attributes = $request->get('attributes');
         $pirate->rank = $request->get('rank');
-
         $pirate->save();
-
         return redirect()->back()->with('status', 'Profile saved!');
     }
-
 
     public function removeFromShip($id)
     {
         Log::info('in remove pirate from ship function');
         $pirate = pirate::find($id);
         $pirate -> ship_id = NULL;
-
-//        $black_perl = pirate::where('name', '=', 'Port Royal')->first();
-////        Log::info(var_dump($black_perl));
-//        $black_perl -> treasure_amount = $black_perl -> treasure_amount + $pirate -> treasure_amount;
-
-//        $pirate->treasure_amount = 0;
-//        $formatted_time = Carbon\Carbon::now()->format('Y-m-d H:i:s');
-//        Log::info($formatted_time);
-//        $pirate->attacked_at = $formatted_time;
-
         $pirate->save();
-//        $black_perl->save();
         return redirect()->back()->with('status', 'Pirate was removed from this ship');
     }
-
-
-
 }
