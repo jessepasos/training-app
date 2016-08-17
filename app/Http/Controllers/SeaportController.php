c6788defc174f4aee613ebe53e3cc767f0992299
vagrant <vagrant@homestead>
2016-08-17 21:08:41 +0000

    fixed auth for seaport

--- app/Http/Controllers/SeaportController.php
+++ app/Http/Controllers/SeaportController.php
@@ -20,7 +20,7 @@ class SeaportController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('admin');
     }
 
     /**
