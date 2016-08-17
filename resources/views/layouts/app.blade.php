c063fe7574f231b66274ba2f4e544bcb5b2e4c32
Vincent Chiu <vincent@datanerds.com>
2016-08-17 11:01:24 -0700

    saving to wrong table for admins

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,26 +48,56 @@
             <a class="navbar-brand" href="{{ url('/ship') }}">
                 SHIPS
             </a>
-        </div>
 
-        <div class="collapse navbar-collapse" id="app-navbar-collapse">
-            <!-- Right Side Of Navbar -->
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
-                @if (Auth::guest())
-                    <li><a href="{{ url('/login') }}">Login</a></li>
-                @else
+                @if(Auth::guard('admin')->user())
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ Auth::user()->name }} <span class="caret"></span>
+                            {{ Auth::guard('admin')->user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="{{ url('/admin/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                        </ul>
+                    </li>
+                @elseif(Auth::guard('user')->user())
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            {{ Auth::guard('user')->user()->name }} <span class="caret"></span>
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
                             <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
                         </ul>
                     </li>
+                @else
+                    <li><a href="{{ url('/login') }}">Login User</a></li>
+                    <li><a href="{{ url('/admin/login') }}">Login Admin</a></li>
+                    <li><a href="{{ url('/register') }}">Register User</a></li>
+                    <li><a href="{{ url('/admin/register') }}">Register Admin</a></li>
                 @endif
             </ul>
+        </div>
+
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <!-- Right Side Of Navbar -->
+            {{--<ul class="nav navbar-nav navbar-right">--}}
+                {{--<!-- Authentication Links -->--}}
+                {{--@if (Auth::guest())--}}
+                    {{--<li><a href="{{ url('/login') }}">Login</a></li>--}}
+                {{--@else--}}
+                    {{--<li class="dropdown">--}}
+                        {{--<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">--}}
+                            {{--{{ Auth::user()->name }} <span class="caret"></span>--}}
+                        {{--</a>--}}
+
+                        {{--<ul class="dropdown-menu" role="menu">--}}
+                            {{--<li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>--}}
+                        {{--</ul>--}}
+                    {{--</li>--}}
+                {{--@endif--}}
+            {{--</ul>--}}
         </div>
     </div>
 </nav>
