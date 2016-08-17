9d18955c4fb140cfcd6e5adf75cd878f2bbb7df3
Vincent Chiu <vincent@datanerds.com>
2016-08-17 14:12:33 -0700

    fixed redirect loop

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/the-black-perl');
+            return redirect('/user-not-admin');
         }
 
         return $next($request);
