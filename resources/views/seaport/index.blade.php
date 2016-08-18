9eceecb036faa832ec849019f6130c75ced3def2
Vincent Chiu <vincent@datanerds.com>
2016-08-18 13:50:17 -0700

    can now attack port with ship
    fixed admin view for seaports
    
    cleaned up views more

--- resources/views/seaport/index.blade.php
+++ resources/views/seaport/index.blade.php
@@ -16,31 +16,47 @@
                                 <a href="/seaport/{{ $seaport->id }}">
                                     <span class="glyphicon glyphicon glyphicon-pencil" aria-hidden="true"></span>
                                 </a>
-                                    <div>name: {{ $seaport->name }} </div>
-                                    <div>treasure amount: {{ $seaport -> treasure_amount }}</div>
-                                    <div>user owner: {{$seaport->user['name']}}</div>
+                                <div>name: {{ $seaport->name }} </div>
+                                <div>treasure amount: {{ $seaport -> treasure_amount }}</div>
+                                <div>user owner: {{$seaport->user['name']}}</div>
 
-                                    @if($seaport -> attacked_at == '0000-00-00 00:00:00')
-                                        <div> never attacked before</div>
-                                    @else
-                                        <div>last attacked at: {{ $seaport -> attacked_at }}</div>
-                                    @endif
+                                @if($seaport -> attacked_at == '0000-00-00 00:00:00')
+                                    <div> never attacked before</div>
+                                @else
+                                    <div>last attacked at: {{ $seaport -> attacked_at }}</div>
+                                @endif
 
 
-                                    <?php $ships_in_this_port = $seaport->ships()->get();
-                                $user_ships_in_this_port = $seaport->ships()->where('user_id', '=', Auth::user()->id);
-                                    $ship_name_array = array();
+                                <?php $ships_in_this_port = $seaport->ships()->get();
+                                //                                $user_ships_in_this_port = $seaport->ships()->where('user_id', '=', Auth::user()->id);
+                                $current_user_ships = array();
                                 ?>
+
+
+                                @if(Auth::guard('admin')->user())
                                     <div>ships stationed here:
                                         <ul>
                                             @if($ships_in_this_port != [])
                                                 @foreach($ships_in_this_port as $ship_in_this_port)
+                                                    <li> {{$ship_in_this_port->name}} </li>
+                                                @endforeach
+                                            @else
+                                                <div>no ships here</div>
+                                            @endif
+                                        </ul>
+                                    </div>
+                                @endif
 
+                                @if(Auth::guard('user')->user())
+                                    <div>ships stationed here:
+                                        <ul>
+                                            @if($ships_in_this_port != [])
+                                                @foreach($ships_in_this_port as $ship_in_this_port)
                                                     @if($ship_in_this_port->user_id == Auth::user()->id)
-                                                        <?php $ship_name_array[$ship_in_this_port->id] = $ship_in_this_port->name; ?>
-                                                            <li>{{$ship_in_this_port->name}} (your ship)</li>
-                                                        @else
-                                                            <li>{{$ship_in_this_port->name}} (other's ship) </li>
+                                                        <?php $current_user_ships[$ship_in_this_port->id] = $ship_in_this_port->name; ?>
+                                                        <li>* {{$ship_in_this_port->name}} (your ship)</li>
+                                                    @else
+                                                        <li>{{$ship_in_this_port->name}} (other's ship)</li>
                                                     @endif
                                                 @endforeach
                                             @else
@@ -49,37 +65,27 @@
                                         </ul>
                                     </div>
 
+                                    @if($current_user_ships != [])
+                                        @if($seaport -> user_id != Auth::user()->id)
+                                            {{ Form::open(array('url' => '/seaport/' . $seaport -> id . '/attack')) }}
+                                            {{ csrf_field() }}
+                                            {{Form::select('ship_id', $current_user_ships)}}
+                                            <button type="submit" class="btn btn-danger">Attack this port!</button>
+                                            {{ Form::close() }}
 
+                                        @else
 
-
-                                @foreach($ship_name_array as $temp_ship_name)
-                                            {{$temp_ship_name}}
-                                @endforeach
-
-                                @if(Auth::guard('user')->user())
-                                    @if($seaport -> user_id != Auth::user()->id)
-                                        {{ Form::open(array('url' => '/seaport/' . $seaport -> id . '/attack')) }}
-                                        {{ csrf_field() }}
-                                        {{Form::select('ship_name', $ship_name_array)}}
-
-                                        <button type="submit" class="btn btn-danger">Attack this port!</button>
-                                        {{ Form::close() }}
-
-                                    @else
-                                        <button type="submit" class="btn btn-success" color="green">Deposit money in
-                                            your
-                                            port!
-                                        </button>
+                                            {{Form::select('ship_id', $current_user_ships)}}
+                                            <button type="submit" class="btn btn-success" color="green">Deposit money in
+                                                your
+                                                port!
+                                            </button>
+                                        @endif
                                     @endif
-
                                 @endif
                             </li>
                         @endforeach
-
                     </div>
-
-                    {{--<div class="panel-footer">--}}
-                    {{--</div>--}}
                 </div>
             </div>
         </div>
