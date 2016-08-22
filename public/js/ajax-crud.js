a62043bdbb4f3ea62be9b9ec5ddd7f2c089a2891
Vincent Chiu <vincent@datanerds.com>
2016-08-22 13:56:39 -0700

    can now display treasure timer on every page

--- public/js/ajax-crud.js
+++ public/js/ajax-crud.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-    setInterval("ajaxd()",100);
+    setInterval("ajaxd(seaport_id)",100);
 });
 
 function ajaxd() {
     $.ajax({
         type: "GET",
-        url: "time/1",
+        url: "time/" + seaport_id,
         // data: "user=success",
         success: function(result){
             $("#div1").empty().append(result['timeSinceLastAction']);
             // console.log(result['updated_at']);
-            console.log(result['timeSinceLastAction']);
+            // console.log(result['timeSinceLastAction']);
         }
     });
 
--- resources/views/seaport/show.blade.php
+++ resources/views/seaport/show.blade.php
@@ -1,8 +1,21 @@
 @extends('seaport.form')
 
 {{--@yield('time_since_last_action')--}}
+<?php  $thing = 10; ?>
+
+
 
 @section('time_since_last_action')
+    {{$seaport->id}}
+    <script>
+
+    var myvar = "<?php echo $thing; ?>";
+    var seaport_id = "<?php echo $seaport->id; ?>";
+    //    console.log(myvar);
+    console.log(seaport_id);
+
+    </script>
+
 <div>time since last action: {{$time_since_last_action}}</div>
 <div id = "div1">cool</div>
 <div>num 15 second intervals since last action: {{$numTimeIntervals}}</div>
